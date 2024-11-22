c7adc524952ca91dd1cb1426f80e478140b0f572
joseph <josephencarnacion62@gmail.com>
2024-11-22 19:51:32 +0800

    asd

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,7 @@ app.use(cors({ origin: 'https://newdispatching.onrender.com', methods: ['POST', 
 // Serve static files from the React app (only in production)
 
 
-  app.use(express.static(path.join(__dirname, 'frontend','build')));
-
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 const responseTeamLocations = {}; 
 const SALT_ROUNDS = 10; // Define the number of salt rounds for bcrypt hashing
@@ -224,8 +223,10 @@ app.get('/api/notifications/:userId', async (req, res) => {
 // Catch-all route to serve React app
 // Catch-all route to serve React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend','build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
   
 app.listen(API_PORT, () => console.log(`Server is running on port ${API_PORT}`));
+console.log('Serving static files from:', path.join(__dirname, '../frontend/build'));
+console.log('Serving index.html from:', path.join(__dirname, '../frontend/build/index.html'));
